c075aa580655c50d6f93c253d45eacbf9fc11ac8
Kasra Karami <Kasra_K2K@yahoo.com>
2023-01-23 11:17:24 +0330

    Create router #3
    Co-Authored-By: Kaveh Karami <48356643+KavehKarami@users.noreply.github.com>
    Co-Authored-By: kasrakaramikk <122204823+kasrakaramikk@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const compression = require("compression");
+const {
+  create,
+  findAll,
+  findOne,
+  update,
+  upsert,
+  deleteOne,
+} = require("./crud.js");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(compression());
+
+app.post("/create", create);
+app.get("/findAll", findAll);
+app.get("/findOne", findOne);
+app.patch("/update", update);
+app.put("/upsert", upsert);
+app.delete("/delete", deleteOne);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
