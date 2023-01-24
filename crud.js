49bdc0884fa4d62b35b6543d027d9066d8a492fb
Kasra Karami <Kasra_K2K@yahoo.com>
2023-01-24 06:45:06 +0330

    Add replace function

--- crud.js
+++ crud.js
@@ -1,64 +1,82 @@
-const { MongoClient, ObjectId } = require("mongodb");
-const mongoClient = new MongoClient("mongodb://0.0.0.0:27017/flex_backend");
+const { MongoClient, ObjectId } = require("mongodb")
+const mongoClient = new MongoClient("mongodb://0.0.0.0:27017/flex_backend")
 
 async function create(req, res) {
-  const { database, collection, doc } = req.body;
+  const { database, collection, doc } = req.body
   const result = await mongoClient
     .db(database)
     .collection(collection)
-    .insertOne(doc);
-  return res.json({ result });
+    .insertOne(doc)
+  return res.json({ result })
 }
 
 async function findAll(req, res) {
-  const { database, collection, selector } = req.body;
-  if ("_id" in selector) selector._id = new ObjectId(selector._id);
+  const { database, collection, selector } = req.body
+  if ("_id" in selector) selector._id = new ObjectId(selector._id)
   const result = await mongoClient
     .db(database)
     .collection(collection)
     .find(selector)
-    .toArray();
-  return res.json({ count: result.length, result });
+    .toArray()
+  return res.json({ count: result.length, result })
 }
 
 async function findOne(req, res) {
-  const { database, collection, selector } = req.body;
-  if ("_id" in selector) selector._id = new ObjectId(selector._id);
+  const { database, collection, selector } = req.body
+  if ("_id" in selector) selector._id = new ObjectId(selector._id)
   const result = await mongoClient
     .db(database)
     .collection(collection)
-    .findOne(selector);
-  return res.json({ result });
+    .findOne(selector)
+  return res.json({ result })
 }
 
 async function update(req, res) {
-  const { database, collection, selector, doc } = req.body;
-  if ("_id" in selector) selector._id = new ObjectId(selector._id);
+  const { database, collection, selector, doc } = req.body
+  if ("_id" in selector) selector._id = new ObjectId(selector._id)
   const result = await mongoClient
     .db(database)
     .collection(collection)
-    .updateOne(selector, { $set: doc }, { upsert: false });
-  return res.json({ result });
+    .updateOne(selector, { $set: doc }, { upsert: false })
+  return res.json({ result })
 }
 
 async function upsert(req, res) {
-  const { database, collection, selector, doc } = req.body;
-  if ("_id" in selector) selector._id = new ObjectId(selector._id);
+  const { database, collection, selector, doc } = req.body
+  if ("_id" in selector) selector._id = new ObjectId(selector._id)
   const result = await mongoClient
     .db(database)
     .collection(collection)
-    .updateOne(selector, { $set: doc }, { upsert: true });
-  return res.json({ result });
+    .updateOne(selector, { $set: doc }, { upsert: true })
+  return res.json({ result })
+}
+
+async function replaceOne(req, res) {
+  const { database, collection, selector, doc } = req.body
+  if ("_id" in selector) selector._id = new ObjectId(selector._id)
+  const result = await mongoClient
+    .db(database)
+    .collection(collection)
+    .replaceOne(selector, doc)
+  return res.json({ result })
 }
 
 async function deleteOne(req, res) {
-  const { database, collection, selector } = req.body;
-  if ("_id" in selector) selector._id = new ObjectId(selector._id);
+  const { database, collection, selector } = req.body
+  if ("_id" in selector) selector._id = new ObjectId(selector._id)
   const result = await mongoClient
     .db(database)
     .collection(collection)
-    .deleteOne(selector);
-  return res.json({ result });
+    .deleteOne(selector)
+  return res.json({ result })
 }
 
-module.exports = { create, findAll, findOne, update, upsert, deleteOne };
+module.exports = {
+  create,
+  findAll,
+  findOne,
+  update,
+  upsert,
+  replaceOne,
+  deleteOne,
+}
--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const cors = require("cors");
-const compression = require("compression");
+const express = require("express")
+const cors = require("cors")
+const compression = require("compression")
 const {
   create,
   findAll,
   findOne,
   update,
   upsert,
+  replaceOne,
   deleteOne,
-} = require("./crud.js");
+} = require("./crud.js")
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(compression());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(compression())
 
-app.post("/create", create);
-app.get("/findAll", findAll);
-app.get("/findOne", findOne);
-app.patch("/update", update);
-app.put("/upsert", upsert);
-app.delete("/delete", deleteOne);
+app.post("/create", create)
+app.get("/findAll", findAll)
+app.get("/findOne", findOne)
+app.patch("/update", update)
+app.put("/upsert", upsert)
+app.put("/replaceOne", replaceOne)
+app.delete("/delete", deleteOne)
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
